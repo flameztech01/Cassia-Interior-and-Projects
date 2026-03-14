6cf65871244a46e2074a99b80ff3c0ed011408d1
flameztech01 <flameztech01@gmail.com>
2026-03-14 12:38:31 +0100

    loading

--- frontend/src/screens/ProfessionalBio.tsx
+++ frontend/src/screens/ProfessionalBio.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom"; // Add this if using React Router
 import BioNavbar from "../bioComponents/bioNavbar"
 import BioHero from "../bioComponents/BioHero"
 import ProfessionalSummary from "../bioComponents/ProfessionalSummary"
@@ -7,6 +9,25 @@ import LeadershipRole from "../bioComponents/LeadershipRole"
 import ContactStrip from "../bioComponents/ContactStrip"
 
 const ProfessionalBio = () => {
+  const location = useLocation(); // Add this if using React Router
+
+  useEffect(() => {
+    // Scroll to top
+    window.scrollTo(0, 0);
+    
+    // If there's a hash in the URL, remove it
+    if (window.location.hash) {
+      window.history.replaceState(null, '', window.location.pathname);
+    }
+    
+    // Prevent browser from restoring scroll position
+    history.scrollRestoration = 'manual';
+    
+    return () => {
+      history.scrollRestoration = 'auto';
+    };
+  }, [location.pathname]); // Re-run when pathname changes
+
   return (
     <div className="relative">
       <BioNavbar />
